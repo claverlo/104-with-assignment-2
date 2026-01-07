d64c1dde98fb7949f1b5485a2c2399551d862f12
claverlo <leopac84@icloud.com>
2026-01-06 22:09:29 -0800

    Update project

--- scripts/salon.js
+++ scripts/salon.js
@@ -1,23 +1,21 @@
 let salon = {
-    name: "Happy Paws Pet Salon",
-    address: {
-        street: "123 Main Street",
-        city: "San Diego, CA",
-        zip: "1234"
-    },
-    hours: {
-        open: "9:00 AM",
-        close: "6:00 PM"
-    }
+name: "Pet Salon",
+address: {
+    street: "123 Main Street",
+    city: "San Diego, CA",
+    zip: "92110"
+},
+hours: {
+    open: "9:00 AM",
+    close: "9:00 PM"
+}
 };
 
-function displaySalonInfo(){
-    let p = document.getElementById("SalonInformation");
+function displaySalonInfo() {
+let p = document.getElementById("SalonInformation");
 
-    p.textContent = `Welcome to ${salon.name}! 
-We are located at ${salon.address.street}, ${salon.address.city} ${salon.address.zip}. 
-Our hours of operation are from ${salon.hours.open} to ${salon.hours.close}. 
-You can contact us at 111-555-1234.`;
+p.textContent = `Welcome to ${salon.name}.
+We are located at ${salon.address.city}, ${salon.address.street}.`;
 }
 
 displaySalonInfo();
